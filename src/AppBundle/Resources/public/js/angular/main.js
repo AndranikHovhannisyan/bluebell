978e878c50cc708b603429b52d764f050e6eb9e9
Hazarapet Tunanyan <hazarapettunanyan@gmail.com>
2016-07-31 22:59:16 +0400

    filters

--- src/AppBundle/Resources/public/js/angular/main.js
+++ src/AppBundle/Resources/public/js/angular/main.js
@@ -4,5 +4,7 @@ angular.module('main', [])
   .controller('MainCtrl', ['$scope', function($scope){
     console.log("hello Ctrl");
 
-    angular.element('.select-filter').select2();
+    angular.element('.category1').select2();
+    angular.element('.category2').select2();
+    angular.element('.category3').select2();
   }]);
