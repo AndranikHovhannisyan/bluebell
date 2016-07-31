d57f40532864c1e403008b1ea8a92dc3b639b4ae
Hazarapet Tunanyan <hazarapettunanyan@gmail.com>
2016-07-31 23:28:50 +0400

    filters

--- src/AppBundle/Resources/public/js/angular/main.js
+++ src/AppBundle/Resources/public/js/angular/main.js
@@ -4,7 +4,15 @@ angular.module('main', [])
   .controller('MainCtrl', ['$scope', function($scope){
     console.log("hello Ctrl");
 
-    angular.element('.category1').select2();
-    angular.element('.category2').select2();
-    angular.element('.category3').select2();
+    angular.element('.category1').select2({
+      placeholder: "Տեսակ"
+    });
+    angular.element('.category2').select2({
+      placeholder: "Ծաղիկներ"
+
+    });
+    angular.element('.category3').select2({
+      placeholder: "Գույներ"
+
+    });
   }]);
