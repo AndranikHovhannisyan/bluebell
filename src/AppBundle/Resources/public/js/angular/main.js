c062db1a955165741389d4e6048cd5649aa346f1
Hazarapet Tunanyan <hazarapettunanyan@gmail.com>
2016-08-01 18:43:22 +0400

    ngMaterial

--- src/AppBundle/Resources/public/js/angular/main.js
+++ src/AppBundle/Resources/public/js/angular/main.js
@@ -1,27 +1,8 @@
 'use strict';
 
-angular.module('main', [])
+angular.module('main', ['ngMaterial', 'ngAnimate'])
   .controller('MainCtrl', ['$scope', '$timeout', function($scope, $timeout){
     console.log("hello Ctrl");
 
-    angular.element('.category1').select2({
-      placeholder: "Տեսակ"
-    });
-    angular.element('.category2').select2({
-      placeholder: "Ծաղիկներ"
-
-    });
-    angular.element('.category3').select2({
-      placeholder: "Գույներ",
-      templateResult: function (item) {
-        if (!item.id) { return item.text; }
-        return $('<p>'+item.text+'<span class="color" style="background-color: red"></span></p>');
-      },
-      templateSelection: function(item){
-        if (!item.id) { return item.text; }
-        return $('<span>'+item.text+'<span class="color" style="background-color: red"></span></span>');
-
-      }
-
-    });
+    $scope.fruitNames = ['Apple', 'Banana', 'Orange'];
   }]);
